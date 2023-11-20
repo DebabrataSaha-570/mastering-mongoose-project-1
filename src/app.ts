2d61411e6100ccd9de2ebce18e49ca7756e534e4
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-11-20 09:54:27 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/config/modules/student/student.route';
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+//api/v1/students/create-student
+
+//application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   const a = 10;
 
   res.send(a);
-});
+};
+
+app.get('/', getAController);
 
 export default app;
--- src/app/config/modules/student/student.model.ts
+++ src/app/config/modules/student/student.model.ts
@@ -83,4 +83,4 @@ const studentSchema = new Schema<Student>({
   isActive: ['active', 'blocked'],
 });
 
-const Student = model<Student>('Student', studentSchema);
+export const StudentModel = model<Student>('Student', studentSchema);
